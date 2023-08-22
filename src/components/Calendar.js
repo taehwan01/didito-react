f7dea7468b3855a9427509150290d731e28e4714
Monte <monte6198@gmail.com>
2023-08-22 12:20:54 +0900

    feat: 달력 UI 및 기능 추가

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,26 +1,54 @@
 import React, { useState } from 'react';
 
+import styles from './Calendar.module.scss';
+
 const daysInMonth = (year, month) => new Date(year, month + 1, 0).getDate();
 
 const Calendar = () => {
   const currentDate = new Date();
   const [year, setYear] = useState(currentDate.getFullYear());
   const [month, setMonth] = useState(currentDate.getMonth());
+  const [selectedDate, setSelectedDate] = useState(new Date().getDate());
 
   const daysCount = daysInMonth(year, month);
   const startDay = new Date(year, month, 1).getDay();
 
-  const daysArray = Array.from({ length: daysCount }, (_, i) => i + 1);
-  const blankDaysArray = Array.from({ length: startDay }, (_, i) => '');
+  const goToPreviousMonth = () => {
+    setMonth((prevMonth) => prevMonth - 1);
+    if (month === 0) {
+      setYear((prevYear) => prevYear - 1);
+      setMonth(11);
+    }
+  };
+
+  const goToNextMonth = () => {
+    setMonth((prevMonth) => prevMonth + 1);
+    if (month === 11) {
+      setYear((prevYear) => prevYear + 1);
+      setMonth(0);
+    }
+  };
+
+  const handleClickDate = (dayNumber) => {
+    setSelectedDate(dayNumber);
+  };
 
   return (
-    <div>
-      <h2>
-        {year}년 {month + 1}월
-      </h2>
-      <table>
-        <thead>
-          <tr>
+    <div className={styles.calendar}>
+      <div className={styles.calendarHeader}>
+        <button className={styles.monthChangeButton} onClick={goToPreviousMonth}>
+          {'<'}
+        </button>
+        <span className={styles.yearAndMonth}>
+          {year}년 {month + 1}월
+        </span>
+        <button className={styles.monthChangeButton} onClick={goToNextMonth}>
+          {'>'}
+        </button>
+      </div>
+      <table className={styles.calendarTable}>
+        <thead className={styles.weekdayHeader}>
+          <tr className={styles.daysHeader}>
             <th>일</th>
             <th>월</th>
             <th>화</th>
@@ -31,13 +59,21 @@ const Calendar = () => {
           </tr>
         </thead>
         <tbody>
-          {Array.from({ length: (daysCount + startDay) / 7 }, (_, weekIndex) => (
+          {Array.from({ length: Math.ceil((daysCount + startDay) / 7) }, (_, weekIndex) => (
             <tr key={weekIndex}>
               {Array.from({ length: 7 }, (_, dayIndex) => {
                 const dayNumber = weekIndex * 7 + dayIndex - startDay + 1;
                 const isCurrentMonth = dayNumber > 0 && dayNumber <= daysCount;
 
-                return <td key={dayIndex}>{isCurrentMonth ? dayNumber : ''}</td>;
+                return (
+                  <th
+                    className={`${styles.date} ${isCurrentMonth && dayNumber === selectedDate ? styles.selected : ''}`}
+                    key={dayIndex}
+                    onClick={() => handleClickDate(dayNumber)}
+                  >
+                    {isCurrentMonth ? dayNumber : ''}
+                  </th>
+                );
               })}
             </tr>
           ))}
